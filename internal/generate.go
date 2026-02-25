54b8ed2f8b966ada7fc97497c0c53d1e3cf4a8c5
Miguel Martín <mmartinv@redhat.com>
2026-02-25 15:22:39 +0100

    feat: add rendezvous TO2 address management API definition
    Introduces OpenAPI specification for the /v1/rvto2addr endpoint to
    configure rendezvous TO2 addresses used during device onboarding.
    Includes CRUD operations with pagination and filtering. Generates
    Go server handlers using oapi-codegen.
    
    Signed-off-by: Miguel Martín <mmartinv@redhat.com>

--- internal/generate.go
+++ internal/generate.go
@@ -4,3 +4,4 @@ package internal
 //go:generate go tool oapi-codegen -config ../configs/goapi-codegen/health.yaml ../api/definitions/health.yaml
 //go:generate go tool oapi-codegen -config ../configs/goapi-codegen/device-ca.yaml ../api/definitions/device-ca.yaml
 //go:generate go tool oapi-codegen -config ../configs/goapi-codegen/rvinfo.yaml ../api/definitions/rvinfo.yaml
+//go:generate go tool oapi-codegen -config ../configs/goapi-codegen/rvto2addr.yaml ../api/definitions/rvto2addr.yaml
