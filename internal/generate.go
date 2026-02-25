ebae5a7d36e261deb5e5322fab89d98e507eda47
Miguel Martín <mmartinv@redhat.com>
2026-02-25 15:21:56 +0100

    feat: add rendezvous info management API definition
    Introduces OpenAPI specification for the /v1/rvinfo endpoint to manage
    rendezvous information configuration. Includes CRUD operations with
    pagination, filtering, and sorting. Generates Go server handlers using
    oapi-codegen.
    
    Signed-off-by: Miguel Martín <mmartinv@redhat.com>

--- internal/generate.go
+++ internal/generate.go
@@ -3,3 +3,4 @@ package internal
 //go:generate go tool oapi-codegen -config ../configs/goapi-codegen/components.yaml ../api/definitions/components.yaml
 //go:generate go tool oapi-codegen -config ../configs/goapi-codegen/health.yaml ../api/definitions/health.yaml
 //go:generate go tool oapi-codegen -config ../configs/goapi-codegen/device-ca.yaml ../api/definitions/device-ca.yaml
+//go:generate go tool oapi-codegen -config ../configs/goapi-codegen/rvinfo.yaml ../api/definitions/rvinfo.yaml
